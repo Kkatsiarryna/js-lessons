bd6a4c4e161874ca2e8b04cc06d046632c2a52f2
Kkatsiarryna <katepicastor@gmail.com>
2024-07-14 16:28:56 +0300

    homework #4

--- objectProperty.js
+++ objectProperty.js
@@ -231,5 +231,237 @@
 // const result2 = [... new Set(numbers)];
 // console.log(result2)
 
+///////////////////////////////////////////////
+///HOMEWORK
+//3
+// let student = {
+// 	name: 'Jonh',
+// 	age: 20,
+// 	course: 'Math'
+// }
+// let propertyCount = Object.keys(student).length;
+// console.log(propertyCount);
 
 
+//4
+// let scores = {
+// 	math: 90,
+// 	english: 85,
+// 	science:92
+// }
+// let arrayValues = Object.values(scores);
+// let sumValues = arrayValues.reduce( (acc, item) => acc + item )
+// console.log(sumValues);
+
+
+// let tottalScrore  =  0;
+// for (let key in scores){
+// 	tottalScrore += scores[key]
+// }
+// console.log(tottalScrore)
+
+//5
+// let obj = { a:1, b:2, c:3};
+// function objectToArray(obj) {
+// 	return Object.entries(obj);
+// }
+// console.log(objectToArray(obj));
+
+//6
+// let students = new Map();
+// students.set('Katya', 10);
+// students.set('Lena', 9);
+// students.set('Sasha', 8);
+// students.set('Nastya', 7);
+
+// console.log(students.get('Katya'));
+
+//7
+// let products = new Map([
+// 	['Milk', 1.2],
+// 	['Bread', 1.5],
+// 	['Cheese', 2.5],
+// ]);
+// if (products.has('Bread')) {
+// 	products.delete('Bread');
+// }
+// console.log(products);
+
+//8
+// let fruits = new Set(['apple', 'banana', 'mango']);
+
+// if(fruits.has('banana')){
+// 	fruits.delete('banana')
+// }
+// console.log(fruits);
+
+//9
+// const numbers = [1,1,4,6,7,8,9,0,0,10,10,11];
+// let result = [...new Set(numbers)];
+// console.log(result);
+
+
+////////////////////////////////////////////////////////////
+//HOMEWORK SPRINT 1
+//1
+// const superUser = {
+//     id: 10,
+//     name: 'Clementina DuBuque',
+//     username: 'Moriah.Stanton',
+//     email: 'Rey.Padberg@karina.biz',
+//     address: {
+//         street: 'Kattie Turnpike',
+//         suite: 'Suite 198',
+//         city: 'Lebsackbury',
+//         zipcode: '31428-2261',
+//         geo: {
+//             lat: '-38.2386',
+//             lng: '57.2232',
+//         },
+//     },
+//     phone: '024-648-3804',
+//     website: 'ambrose.net',
+//     company: {
+//         name: 'Hoeger LLC',
+//         catchPhrase: 'Centralized empowering task-force',
+//         bs: 'target end-to-end models',
+//     },
+// };
+//
+// const result = {...superUser, address: {...superUser.address, geo:{...superUser.address.geo, lat: 'test coordinate'}}};
+//
+// const result2 = structuredClone(superUser);
+// result2.address.geo.lat = 'hello'
+//
+// console.log(superUser)
+// console.log(result);
+// console.log(result2);
+
+
+//2
+// const sumAndDifference = (a, b) => {
+//     return {
+//         sum: a + b,
+//         difference: a - b,
+//     }
+// }
+// const {sum, difference} = sumAndDifference(5, 2);
+//
+// console.log("Сумма: " + sum);
+// console.log("Разность: " + difference);
+
+//3
+// const numbers = [-1,2,-3,4,-5,6];
+// const getPositiveNumbers = (numbers) => {
+// 	return numbers.filter( el => el > 0)
+// }
+// console.log(getPositiveNumbers(numbers));
+
+//4
+// const arr1 = [1,2,3];
+// const arr2 = [4,5,6];
+// const mergeArrays = (...arrays) => {
+// 	return arrays.reduce( (acc, arr) => acc.concat(arr), [])
+// }
+// console.log(mergeArrays(arr1, arr2));
+
+//5
+// const strings = ['apple', 'banana', 'orange'];
+// const findString = (array, word) => {
+// 	return array.includes(word)
+// }
+// console.log(findString(strings, 'apple'));
+// console.log(findString(strings, 'tomato'));
+
+//6
+// const people = [
+//     { name: 'Alice', age: 30 },
+//     { name: 'Bob', age: 25 },
+//     { name: 'Charlie', age: 35 },
+// ];
+// function sortByProperty(arr, propertyName){
+//     return arr.sort((a, b) => a.propertyName - b.propertyName );
+// }
+// sortByProperty(people, 'age');
+// console.log(people);
+
+//7
+// function extractSubarray (arr, startIndex, endIndex) {
+//      const extracted =  arr.slice(startIndex, endIndex);
+//      arr.splice(startIndex, extracted.length);
+//      return extracted;
+// }
+//
+// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// let extracted = extractSubarray(array, 2, 6);
+// console.log(extracted); // Вывод: [3, 4, 5, 6]
+// console.log(array); // Вывод: [1, 2, 7, 8, 9]
+
+//8
+// const addPropertyToObject = (objectName, property, value) => {
+//     objectName[property] = value;
+// }
+// let obj = { name: 'Alice' };
+// addPropertyToObject(obj, 'age', 30);
+// console.log(obj); // Вывод: { name: "Alice", age: 30 }
+
+//9
+// const students = {
+//     Alice: [4, 5, 3],
+//     Bob: [3, 2, 4],
+//     Charlie: [5, 5, 5]
+// };
+//
+// const formatStudentGrades = (obj) => {
+//     //return Object.entries(obj).map( el => `${el[0]}: ${el[1]}`);
+//     return Object.entries(obj).map( ([name, grade]) => `${name} : ${grade.join(',') }`)
+// }
+// console.log(formatStudentGrades(students));
+
+// Вывод:
+// [
+//   "Alice: 4, 5, 3",
+//   "Bob: 3, 2, 4",
+//   "Charlie: 5, 5, 5"
+// ]
+
+//10
+// const students = {
+//     Alice: [4, 5, 3],
+//     Bob: [3, 2, 4],
+//     Charlie: [5, 5, 5]
+// };
+// function getStudentNames (obj) {
+//     return Object.keys(obj);
+// }
+// console.log(getStudentNames(students));
+// Вывод: ["Alice", "Bob", "Charlie"]
+
+//11
+//возвращает массив уникальных жанров книг\
+// const library = {
+//     'The Great Gatsby': { genre: 'Fiction' },
+//     'To Kill a Mockingbird': { genre: 'Fiction' },
+//     1984: { genre: 'Science Fiction' },
+//     'Pride and Prejudice': { genre: 'Romance' },
+//     'The Catcher in the Rye': { genre: 'Fiction' },
+// };
+// function getUniqueGenres (obj) {
+//    let newArr =  Object.values(obj).map( book => book.genre);
+//    return [...new Set(newArr)]
+// }
+// console.log(getUniqueGenres(library));
+// // Вывод: ["Fiction", "Science Fiction", "Romance"]
+
+//12
+// const pairs = [
+//     ['a', 1],
+//     ['b', 2],
+//     ['c', 3],
+// ];
+//
+// function createMapFromArray (obj) {
+//     return new Map(obj)
+// }
+// console.log(createMapFromArray(pairs));
+// // Вывод: Map { 'a' => 1, 'b' => 2, 'c' => 3 }
