537ad0d9721b4be4a97dc7ad1c83b4e70cab803c
Kkatsiarryna <katepicastor@gmail.com>
2024-12-01 20:17:37 +0300

    Promise tasks

--- Promise-3.js
+++ Promise-3.js
@@ -215,56 +215,479 @@
 
 
 //EXAMPLE 6
-console.log('START')
-let prom = new Promise((resolve, reject) => { //консруктор промиса синхронный
-    console.log('START PROMISE')
-    setTimeout((response) => { //setTimeout асинхронный
-        console.log('START SETTIMEOUT')
-        if(response.httpStatus >= 200 && response.httpStatus < 400) {
-            resolve(response.data); // меняет состояние объекта, далее у объекта вызываются встроенные методы; аргументы приходят методу then
-            console.log('1', prom)
-        }else{
-            reject(response.error)
-        }
+// console.log('START')
+// let prom = new Promise((resolve, reject) => { //консруктор промиса синхронный
+//     console.log('START PROMISE')
+//     setTimeout((response) => { //setTimeout асинхронный
+//         console.log('START SETTIMEOUT')
+//         if(response.httpStatus >= 200 && response.httpStatus < 400) {
+//             resolve(response.data); // меняет состояние объекта, далее у объекта вызываются встроенные методы; аргументы приходят методу then
+//             console.log('1', prom)
+//         }else{
+//             reject(response.error)
+//         }
+//
+//     }, 1000, {httpStatus: 200, data:{userName: 'kate', id: 'jewji11jo', status: 'active'}, error: {}})
+//     console.log('END PROMISE')
+// })
+// console.log('MIDDLE')
+// prom //асинхронный
+//     .then(console.log) //готовый коллбэк
+//
+// console.log('END')
+//
+// // START
+// // START PROMISE
+// // END PROMISE
+// // MIDDLE
+// // END
+// // START SETTIMEOUT
+// // 1 Promise { { userName: 'kate', id: 'jewji11jo', status: 'active' } }
+// // { userName: 'kate', id: 'jewji11jo', status: 'active' }
 
-    }, 1000, {httpStatus: 200, data:{userName: 'kate', id: 'jewji11jo', status: 'active'}, error: {}})
-    console.log('END PROMISE')
-})
-console.log('MIDDLE')
-prom //асинхронный
-    .then(console.log) //готовый коллбэк
+//EXAMPLE finally
 
-console.log('END')
-
-// START
-// START PROMISE
-// END PROMISE
-// MIDDLE
-// END
-// START SETTIMEOUT
-// 1 Promise { { userName: 'kate', id: 'jewji11jo', status: 'active' } }
-// { userName: 'kate', id: 'jewji11jo', status: 'active' }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Promise.resolve(10)
+//     .finally(() => {console.log('finally 1')
+//     return 1005050
+//     })
+//     .then(console.log)
+//     .finally(()=> {console.log('finally 2')})
+//     .catch(console.log)
+//     .finally(() => console.log('finally 3'))
+//
+// // finally 1
+// // 10
+// // finally 2
+// // finally 3
 
 
+//потеря данных
+// Promise.resolve(10)
+//     .finally(() => {console.log('finally 1')
+//       throw 9000
+//     })
+//     .then(console.log)
+//     .finally(()=> {console.log('finally 2')})
+//     .catch(()=> console.log('catch'))
+//     .finally(() => console.log('finally 3'))
+//
+// // finally 1
+// // finally 2
+// // catch
+// // finally 3
 
+
+//ASYNC AWAIT
+
+// console.log('START')
+// async function f() { //f = async() => {}
+//     //такая функция всегда возращает объект промиса
+//     //функция работает синхронно пока не произойдет асинхронность
+//
+//     console.log('MIDDLE')
+// }
+// f()
+// console.log('END')
+//
+// // START
+// // MIDDLE
+// // END
+
+
+// console.log('START')
+// async function f() {
+//     console.log('START F')
+//     const response = await new Promise((resolve, reject) => {console.log('IN PROMISE')});
+//     //промис не поменял состояние с pending
+//     //await останавливает выполнение асинхронного кода в функции, но не останавливает выполнение кода вообще
+//     //код выходит из места вызова и продолжает выполнять весь синхронный код, который есть
+//     //если мироси зарезолвится, возвращается к коду - это микротаска
+//
+//     console.log('MIDDLE')
+// }
+// f()
+// console.log('END')
+//
+// // START
+// // START F
+// // IN PROMISE
+// // END
+
+
+// console.log('START')
+// async function f() {
+//     console.log('START F')
+//     const response = await new Promise((resolve, reject) => {
+//         console.log('IN PROMISE')
+//         reject()
+//
+//     });
+//     console.log('MIDDLE')
+// }
+// f()
+// console.log('END')
+//
+// // START
+// // START F
+// // IN PROMISE
+// // END
+// // // code: 'ERR_UNHANDLED_REJECTION' - не обработали
+
+
+
+// console.log('START')
+// async function f() {
+//     try {
+//         console.log('START F')
+//         const response = await new Promise((resolve, reject) => {
+//             console.log('IN PROMISE')
+//             reject('!!!!!!')
+//
+//         });
+//         console.log('MIDDLE')
+//     } catch (error) {
+//         console.error(error)
+//     }
+// }
+// f()
+// console.log('END')
+//
+// // START
+// // START F
+// // IN PROMISE
+// // END
+// // !!!!!!
+
+
+// console.log('START')
+// async function f() {
+//     try {
+//         console.log('START F')
+//         const response = await new Promise((resolve, reject) => {
+//             console.log('IN PROMISE')
+//             resolve('!!!!!!')
+//
+//         });
+//         console.log('MIDDLE')
+//     } catch (error) {
+//         console.error(error)
+//     }
+// }
+// f()
+// console.log('END')
+//
+// // START
+// // START F
+// // IN PROMISE
+// // END
+// // MIDDLE
+
+
+//EXAMPLE
+//Task 1
+
+// setTimeout( ()=> console.log(1), 0);
+// console.log(2);
+// ( () => console.log(3))();
+// Promise.resolve(console.log(4)); // 4 поместится в промис
+
+//resolve это функция которая в качестве аргумента принимает console.log(4), которая фунцкия и приняла аргумент 4
+//console.log(4) - отработает синхронно, результат undefined уходит в качестве аргумента resolve, дальше создается промис resolve со значением undefined
+
+// 2
+// 3
+// 4
+// 1
+
+// 1.setTimeout(() => console.log(1), 0):
+// •  Этот код ставит функцию в очередь на выполнение после всех синхронных операций, даже если задержка равна 0. Поэтому 1 будет выведено позже.
+// 2.console.log(2):
+// •  Этот вызов выполняется синхронно и сразу выводит 2 в консоль.
+// 3.(() => console.log(3))():
+// •  Это немедленно вызываемая функция (IIFE), которая синхронно выводит 3 в консоль.
+// 4.Promise.resolve(console.log(4)):
+// •  Сначала выполняется console.log(4), который синхронно выводит 4 в консоль.
+// •  Затем промис разрешается, но так как нет then или catch, это не влияет на вывод.
+
+
+// setTimeout( ()=> console.log(1), 0);
+// console.log(2);
+// ( () => console.log(3))();
+// Promise.resolve(console.log(4))
+//     .then( res => console.log(res))
+//
+// // 2
+// // 3
+// // 4
+// // undefined
+// // 1
+
+
+// setTimeout( ()=> console.log(1), 0);
+// console.log(2);
+// ( () => console.log(3))();
+// Promise.resolve(() => {console.log(4)})
+//     .then( res => console.log(res))
+//
+// // 2
+// // 3
+// //     [Function (anonymous)]
+// // 1
+
+
+// setTimeout( ()=> console.log(1), 0);
+// console.log(2);
+// ( () => console.log(3))();
+// Promise.resolve(2+3)
+//     .then( res => console.log(res))
+//
+// // 2
+// // 3
+// // 5
+// // 1
+
+
+//TASK2
+// new Promise ( ( resolve, reject ) => { //конструтор промиса работает синхронно
+//     console.log(1)
+// });
+// new Promise ( ( resolve, reject ) => {
+//     setTimeout( () => console.log(2), 0)
+// });
+// Promise.resolve(setTimeout( () => console.log(3), 0));
+// console.log(4);
+// Promise.reject(console.log(5))
+//     .catch( err => console.log(6));
+//
+// // 1
+// // 4
+// // 5
+// // 6
+// // 2
+// // 3
+
+//TASK 3
+// (function(){
+//     setTimeout( ()=> console.log(1), 100)
+// })();
+// console.log(2);
+// new Promise( ( resolve, reject ) => {
+//     setTimeout( ()=> console.log(3), 50)
+// })
+//
+// function f(){
+//     console.log(4)
+// }
+//
+// Promise.resolve(console.log(5))
+//     .then(()=> console.log(6))
+//
+// console.log(7)
+//
+// // 2
+// // 5
+// // 7
+// // 6
+// // 3
+// // 1
+
+
+// (function(){
+//         setTimeout( ()=> console.log(1), 100)
+//     })();
+// console.log(2);
+// new Promise( ( resolve, reject ) => {
+//     setTimeout( ()=> console.log(3), 50)
+// })
+//
+// function f(){
+//     console.log(4)
+// }
+//
+// Promise.resolve(() => {console.log(5)})
+//     .then(()=> console.log(6))
+//
+// console.log(7)
+//
+//
+// // 2
+// // 7
+// // 6
+// // 1
+// // 3
+
+
+//TASK 4
+
+// function f(num){
+//     console.log(num)
+// };
+// Promise.resolve(1)
+//     .then(f);
+//
+// (function (){
+//     console.log(2)
+// })();
+//
+// console.log(3);
+//
+// new Promise( (resolve,reject) => {
+//     console.log(4)
+// })
+//
+// setTimeout(f, 0, 5)
+//
+//
+// // 2
+// // 3
+// // 4
+// // 1
+// // 5
+
+
+//TASK 5
+
+// (function(){
+//         setTimeout( ()=> console.log(1), 100)
+//     })();
+// console.log(2);
+//
+// let i = 0;
+// while(i<5000000000){ //код тут остановится
+//     i++
+// }
+//
+// new Promise( ( resolve, reject ) => {
+//     setTimeout( ()=> console.log(3), 50)
+// })
+//
+// function f(){
+//     console.log(4)
+// }
+//
+// Promise.resolve(console.log(5))
+//
+// // 2
+// // 5
+// // 1
+// // 3
+
+
+//TASK 6
+
+// async function sleep(ms){ //фунцкия наружу выбрасывает промис resolve
+//     return new Promise((resolve,reject)=>{
+//         setTimeout( ()=> {
+//             resolve()
+//             console.log(ms)
+//         }, ms*100)
+//     });
+// }
+//
+// async function show(){
+//     await sleep(3);
+//     await sleep(2);
+//     await sleep(1);
+// }
+// show()
+//
+// // 3
+// // 2
+// // 1
+
+
+//TASK 7
+
+// const pr1 = Promise.resolve(10)
+// const pr2 = Promise.resolve(0)
+//
+// pr1
+//     .then( res => {
+//         console.log(res)
+//         return res + 2
+//     })
+//     .then( res => {
+//         console.log(res)
+//         return res + 2
+//     })
+//     .then( res => {
+//         console.log(res)
+//         return res + 2
+//     })
+// pr2
+//     .then( res => {
+//         console.log(res)
+//         return res + 1
+//     })
+//     .then( res => {
+//         console.log(res)
+//         return res + 1
+//     })
+//     .then( res => {
+//         console.log(res)
+//         return res + 1
+//     })
+//
+// // 10
+// // 0
+// // 12
+// // 1
+// // 14
+// // 2
+
+
+// Task 01
+// Создайте промис, который постоянно находиться в состоянии pending.
+// В конструкторе промиса выведите в консоль сообщение "Promise is created".
+
+
+// Task 02
+// Создайте промис, который после создания сразу же переходит в состояние resolve
+// и возвращает строку 'Promise Data'
+// Получите данные промиса и выведите их в консоль
+
+
+// Task 03
+// Создайте промис, который после создания сразу же переходит в состояние rejected
+// и возвращает строку 'Promise Error'
+// Получите данные промиса и выведите их в консоль
+
+
+// Task 04
+// Создайте промис, который переходит в состояние resolved через 3с.
+// (Используйте setTimeout)
+// и возвращает строку 'Promise Data'
+// Получите данные промиса и выведите их в консоль
+
+
+// Task 05
+// Создайте литерал объекта handlePromise со следующими свойствами:
+// promise, resolve, reject, onSuccess, onError
+// Проинициализируйте первые три свойства null,
+// а последние два функциями, которые принимают один параметр и выводят
+// в консоль сообщения: первая - `Promise is resolved with data: ${paramName}`
+// вторая - `Promise is rejected with error: ${paramName}`
+// Создайте три обработчика события click для кнопок "Create Promise", "Resolve Promise", "Reject Promise".
+// Первый обработчик, создает промис, заполняет первые три свойства,
+// описаного выше объекта: свойство promise получает новый созданный промис,
+// свойства resolve и reject получают ссылки на соответствующие функции
+// resolve и reject. Следующие два обработчика запускают методы resolve и reject.
+
+
+// Task 06
+// Создайте промис, который через 1 с возвращает строку "My name is".
+// Создайте функцию onSuccess, которая получает один параметр,
+// прибавляет к нему Ваше имя и возвращает новую строку из функции
+// Создайте функцию print, которая выводит в консоль значение своего параметра
+// Добавьте два метода then и передайте созданные функции.
+
+
+// Task 7
+// Создайте три промиса. Первый промис возвращает объект { name: "Anna" } через 2с,
+// второй промис возвращает объект {age: 16} через 3 с, а третий {city: ''} через 4с.
+// Получите результаты работы промисов, объедините свойства объектов
+// и выведите в консоль {name, age, city}
 
 
 
